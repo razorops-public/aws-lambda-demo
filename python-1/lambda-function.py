c01271cb258b7e1be814a287fb167bb0c486636d
dsyoi <dsyoi@users.noreply.github.com>
2023-12-11 18:06:53 +0530

    add python folders

---
+++ python-1/lambda-function.py
@@ -0,0 +1,28 @@
+import json
+import requests
+
+def lambda_handler(event, context):
+    # Make sure a Pokemon name (or ID) was passed in the event object:
+    pokemon = event.get('pokemon')
+    if pokemon is None:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Missing pokemon attribute in the event object')
+        }
+
+    # If we have a pokemon name/ID passed in, try to get info for it:
+    r = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
+
+    if r.status_code == 200:
+        status_code = 200
+        body = json.dumps(r.json())
+    else:
+        status_code = r.status_code
+        body = json.dumps(f"Could not load information for Pokemon {pokemon}")
+
+    # Return what we got from the API:
+    return {
+        'statusCode': status_code,
+        'body': body
+    }
+  
---
+++ python-2/lambda_function.py
@@ -0,0 +1,13 @@
+import json
+import boto3
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    logger.info('HelloWorld from Python Lambda!')
+    return {
+        'statusCode': 200,
+        'body': json.dumps('HelloWorld from Python Lambda!')
+    }
